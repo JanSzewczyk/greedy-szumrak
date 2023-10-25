9da51add10268e9235dd583e91aa4394af922596
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-10-25 23:36:33 +0200

    feat(auth-adapter): add `Firestore` adapter

--- env.js
+++ env.js
@@ -15,7 +15,10 @@ const env = createEnv({
       .transform((value) => value === "true" || value === "1"),
     NEXTAUTH_SECRET: z.string().min(10),
     GOOGLE_CLIENT_ID: z.string(),
-    GOOGLE_CLIENT_SECRET: z.string()
+    GOOGLE_CLIENT_SECRET: z.string(),
+    ADMIN_FIREBASE_PROJECT_ID: z.string(),
+    ADMIN_FIREBASE_CLIENT_EMAIL: z.string().email(),
+    ADMIN_FIREBASE_PRIVATE_KEY: z.string().transform((value) => value.replace(/\\n/g, "\n"))
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
@@ -23,7 +26,10 @@ const env = createEnv({
     CI: process.env.CI,
     NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    ADMIN_FIREBASE_PROJECT_ID: process.env.ADMIN_FIREBASE_PROJECT_ID,
+    ADMIN_FIREBASE_CLIENT_EMAIL: process.env.ADMIN_FIREBASE_CLIENT_EMAIL,
+    ADMIN_FIREBASE_PRIVATE_KEY: process.env.ADMIN_FIREBASE_PRIVATE_KEY
   }
 });
 
--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,37 +1,36 @@
-// import { FirestoreAdapter } from "@next-auth/firebase-adapter";
-// import { cert } from "firebase-admin/app";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import { cert } from "firebase-admin/app";
 import { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 import { env } from "~/env";
 
 export const authOptions: NextAuthOptions = {
-  // adapter: FirestoreAdapter({
-  //   credential: cert({
-  //     projectId: env.ADMIN_FIREBASE_PROJECT_ID,
-  //     clientEmail: env.ADMIN_FIREBASE_CLIENT_EMAIL,
-  //     privateKey: env.ADMIN_FIREBASE_PRIVATE_KEY
-  //   })
-  // }),
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: env.ADMIN_FIREBASE_PROJECT_ID,
+      clientEmail: env.ADMIN_FIREBASE_CLIENT_EMAIL,
+      privateKey: env.ADMIN_FIREBASE_PRIVATE_KEY
+    })
+  }),
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID!,
       clientSecret: env.GOOGLE_CLIENT_SECRET!
     })
   ],
-  secret: env.NEXTAUTH_SECRET
-  // session: {
-  //   strategy: "jwt"
-  // },
-  //
-  // callbacks: {
-  //   async session({ session, token }) {
-  //     session.user = {
-  //       ...session.user,
-  //       id: token.sub as string
-  //     };
-  //     session.exp = token.exp as string;
-  //     return session;
-  //   }
-  // }
+  secret: env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt"
+  },
+  callbacks: {
+    async session({ session, token }) {
+      session.user = {
+        ...session.user,
+        id: token.sub as string
+      };
+      session.exp = token.exp as string;
+      return session;
+    }
+  }
 };
