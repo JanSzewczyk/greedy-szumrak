660e56a0f9c83fdab5da0253df4f1ed577daab62
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-10-25 23:04:34 +0200

    feat(auth): set up `next-auth` and add Google provider

--- env.js
+++ env.js
@@ -12,16 +12,18 @@ const env = createEnv({
     CI: z
       .enum(["true", "false", "0", "1"])
       .optional()
-      .transform((value) => value === "true" || value === "1")
-  },
-  client: {
-    // Client env variables, eg:
-    // NEXT_PUBLIC_CLIENT_VAR: z.string(),
+      .transform((value) => value === "true" || value === "1"),
+    NEXTAUTH_SECRET: z.string().min(10),
+    GOOGLE_CLIENT_ID: z.string(),
+    GOOGLE_CLIENT_SECRET: z.string()
   },
   runtimeEnv: {
     NODE_ENV: process.env.NODE_ENV,
     ANALYZE: process.env.ANALYZE,
-    CI: process.env.CI
+    CI: process.env.CI,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET
   }
 });
 
