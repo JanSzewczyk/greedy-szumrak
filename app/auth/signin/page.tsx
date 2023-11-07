c7ee9b68aff4b372c1ea63f6a6bce9ac5a537bec
JanSzewczyk <jan.szewczyk1997@gmail.com>
2023-11-07 14:05:21 +0100

    ci(vercel): set up vercel action v3

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -1,7 +1,7 @@
 import { type Metadata } from "next";
 
-import { Hero } from "./components/hero";
-import { SignInButtons } from "./components/sign-in-buttons";
+import { Hero } from "~/components/components/hero";
+import { SignInButtons } from "~/components/components/sign-in-buttons";
 
 export const metadata: Metadata = {
   title: "Greedy Szumrak - Sign in"
