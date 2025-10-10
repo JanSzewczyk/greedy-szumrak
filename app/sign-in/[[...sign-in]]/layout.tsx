ecd71a292e9253e370cfa2cdfd9774a8dadc9aad
Jan Szewczyk <jan.szewczyk1997@gmail.com>
2025-10-10 22:14:05 +0200

    feat: redesign sign-in page with marketing content and feature highlights

--- app/sign-in/[[...sign-in]]/layout.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export default function SignInLayout({ children }: { children: React.ReactNode }) {
-  return <main className="container flex flex-col items-center py-10 md:py-20">{children}</main>;
-}
